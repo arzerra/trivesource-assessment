6e8c2e5941a8f7a2331c895c95e425821d47abd5
arzerra <ianclark1506@gmail.com>
2025-10-18 03:20:31 +0800

    quote fixed

--- app/page.tsx
+++ app/page.tsx
@@ -200,10 +200,10 @@ export default function Home() {
             <h1>Question 8</h1>
             <p>
               Please attempt to deploy a function on our service. This need not
-              be complicated. It could be "Hello World". Note that failure to
-              deploy is not failing the exercise! Whether you have trouble or
-              not, please describe what you experienced and how you attempted to
-              troubleshoot any issues you encountered.
+              be complicated. It could be &quot;Hello World&quot;. Note that
+              failure to deploy is not failing the exercise! Whether you have
+              trouble or not, please describe what you experienced and how you
+              attempted to troubleshoot any issues you encountered.
             </p>
             <p className="text-green-500">answer:</p>
             <p></p>
